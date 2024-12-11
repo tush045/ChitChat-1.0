2f273d1188333c15e974676e5a4b127c59431496
tush045 <tusharchopra275@gmail.com>
2024-12-12 00:36:44 +0530

    setup Stream Chat provider

---
+++ src/Provders/ChatProvider.tsx
@@ -0,0 +1,42 @@
+import { PropsWithChildren, useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { StreamChat } from "stream-chat";
+import { Chat, OverlayProvider } from "stream-chat-expo";
+
+
+const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_PUBLIC_KEY);
+export default function ChatProvider({ children }: PropsWithChildren) {
+    const [isReady,setIsReady] = useState(false);
+    useEffect(() => {
+        const connect = async () => {
+            await client.connectUser(
+                {
+                    id: "jlahey",
+                    name: "Jim Lahey",
+                    image: "https://i.imgur.com/fR9Jz14.png",
+                },
+                client.devToken('jlahey')
+            );
+            setIsReady(true);
+            // const channel = client.channel("messaging", "the_park", {
+            //     name: "The Park",
+            //   });
+            //   await channel.watch()
+        }
+        connect();
+        return () =>{
+            client.disconnectUser();
+            setIsReady(false);
+        }
+    }, [])
+    if (!isReady) {
+        return <ActivityIndicator color={'grey'} style={{flex:1}}/>;
+    }
+    return (
+        <OverlayProvider>
+            <Chat client={client}>
+                {children}
+            </Chat>
+        </OverlayProvider>
+    )
+}
--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,39 +1,15 @@
+import ChatProvider from "@/src/Provders/ChatProvider";
 import { Slot, Stack } from "expo-router";
 import { useEffect } from "react";
-import { StreamChat } from "stream-chat";
 import { Chat, OverlayProvider } from "stream-chat-expo"
 
 
 
-const client = StreamChat.getInstance("bs6yd8shjhkp");
+
 export default function HomeLayout() {
-    useEffect(() => {
-        const connect = async () => {
-            await client.connectUser(
-                {
-                    id: "jlahey",
-                    name: "Jim Lahey",
-                    image: "https://i.imgur.com/fR9Jz14.png",
-                },
-                client.devToken('jlahey')
-            );
-            // const channel = client.channel("messaging", "the_park", {
-            //     name: "The Park",
-            //   });
-            //   await channel.watch()
-        }
-        connect();
-    }, [])
-
-
-
     return (
-        <OverlayProvider>
-            <Chat client={client}>
-            <Stack screenOptions={{headerShown:false}}/>
-            </Chat>
-        </OverlayProvider>
-        
-        
+        <ChatProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+        </ChatProvider>
     );
 }
