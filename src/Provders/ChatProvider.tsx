2a3c133c81447e68919bca2446c761a3f22afe4b
tush045 <tusharchopra275@gmail.com>
2024-12-12 19:52:34 +0530

    connect supabase avatar to stream chat

--- src/Provders/ChatProvider.tsx
+++ src/Provders/ChatProvider.tsx
@@ -22,7 +22,7 @@ export default function ChatProvider({ children }: PropsWithChildren) {
                 {
                     id: profile?.id,
                     name: profile?.full_name,
-                    image: "https://i.imgur.com/fR9Jz14.png",
+                    image: supabase.storage.from('avatars').getPublicUrl(profile.avatar_url).data.publicUrl,
                 },
                 client.devToken(profile?.id)
             );
--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,7 +1,10 @@
+import { useAuth } from "@/src/Provders/AuthProvider";
 import { router, Stack } from "expo-router";
-import  {ChannelList} from 'stream-chat-expo'
+import { ChannelList } from 'stream-chat-expo'
 
-export default function MainTabScreen(){
-    
-    return <ChannelList  onSelect={(channel) => router.push(`/channel/${channel.cid}`)}/>;
+export default function MainTabScreen() {
+    const {user} = useAuth();
+
+    return <ChannelList filters={{members:{$in:[user?.id]}}}
+    onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />;
 }
